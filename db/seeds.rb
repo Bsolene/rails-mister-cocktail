a8d6261f5eaac22771881b450ee05f26d43a2e61
solene basaia <bsolene@hotmail.fr>
2017-05-25 16:49:42 +0100

    fine by me

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,4 +2,13 @@ require "open-uri"
 require "json"
 
 
+url = 'http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+file = open(url).read
+data_hash = JSON.parse(file)
+
+data_hash['drinks'].each do |ingredient|
+  ingredient_name = ingredient['strIngredient1']
+  Ingredient.create!(name: ingredient_name)
+end
+
 
